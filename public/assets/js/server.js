8a5ee4a610867f605680d9c85e86cd23d8390b83
NIASKA <mkman751@yahoo.com>
2021-05-10 16:44:50 -0500

    added app.get

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,14 +4,26 @@ const db = require('./db/db.json');
 
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-app.use(express.static("./public/"));
 
-require()(app);
-require()(app);
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'notes.html'))
+);
+
+app.get('/index', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
+);
+
+app.get('/api/notes', (req, res) => {
+    return res.json();
+});
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
