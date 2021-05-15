512fc71bd19eb75952073ae92f8c757a689e1fde
NIASKA <mkman751@yahoo.com>
2021-05-15 11:50:17 -0500

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Express-Note-Taker
+# Name of Project
+Note Taker
+
+# About
+Creating an application that uses express to write out notes. It can save and delete just like any other note. 
+Refreshing will not get rid of the storage as it is stored in local storage. 
+
+# Usage
+Node.js, Javascript, FS, Express.js, BootStrap
+
+# License
+![License](https://img.shields.io/badge/license-MIT-blue.svg "License Badge")
+## Find out more clicking link below 
+[License](https://opensource.org/licenses/MIT)
+
+# Links to application
+Link to repo: https://github.com/NIASKAA/Express-Note-Taker \
+Link to Youtube quick walk-through: 
+Link to Heroku: https://niaskaexpressnotetaker.herokuapp.com/
+
+# Contribution 
+None, only me. 
+
+# Github Name
+NIASKAA
+    
--- server.js
+++ server.js
@@ -21,65 +21,6 @@ app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-/*app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
-        if(err) throw err;
-        res.json(JSON.parse(data));
-    });
-});*/
-
-/*app.post('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
-        if(err) throw err;
-        const note = JSON.parse(data);
-        const newNote = [];
-
-        note.push(req.body);
-
-        // UpdateNote function will basically add 1 to the id slot so new notes can all have different id for delete access
-        for(let i = 0; i < note.length; i++){
-
-            const updateNote = {
-                title: note[i].title,
-                text: note[i].text,
-                id: i
-            }
-            newNote.push(updateNote);
-        }
-
-        // Now we append the data from newNote to the db.json
-        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newNote, null, 2), (err) => {
-            if(err) throw err;
-            res.json(req.body);
-        });
-    });
-});*/
-
-/*app.delete('/api/notes/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
-        if(err) throw err;
-        const note = JSON.parse(data);
-        const currentNote = [];
-
-        for(let i = 0; i < note.length; i++){
-            if(i !== id){
-
-                const deleteNote = {
-                    title: note[i].title,
-                    text: note[i].text,
-                    id: currentNote.id
-                };
-                currentNote.push(deleteNote);
-            }
-        }
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(currentNote, null, 2), (err) => {
-            if(err) throw err;
-            res.json(req.body);
-        });
-    });
-});*/
-
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     let updateNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
