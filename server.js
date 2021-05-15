9b743ad1aeeccbbdc614bae1be83f17642c8aa00
NIASKA <mkman751@yahoo.com>
2021-05-15 14:17:26 -0500

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Writing out the necessary ports to connect with html pages
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -21,27 +21,38 @@ app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+// App.post function first reads the db.json file and whenever a new note is created, it will push it with an id
+// After pushing it, it will write to the db.json file as a new note
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
-    let updateNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteList = (noteList.length).toString();
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
 
-    newNote.id = noteList;
-    note.push(updateNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(newNote));
-    res.json(newNote);
+    //create new property called id based on length and assign it to each json object
+    newNote.id = notelength;
+    //push updated note to the data containing notes history in db.json
+    noteList.push(newNote);
 
+    //write the updated data to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
 })
 
+// App.delete function basically checks for the id and uses filter to filter out that id and return the new array list to delete notes
+// After that it does the same thing as app.post and write out only the current notes left
 app.delete("/api/notes/:id", (req, res) => {
-    let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let deleteId = (req.params.id).toString();
-    currentNote = currentNote.filter(selected =>{
-        return selected.id != deleteId;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+    //filter all notes that does not have matching id and saved them as a new array
+    //the matching array will be deleted
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
     })
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(currentNote));
-    res.json(currentNote);
+    //write the updated data to db.json and display the updated note
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
 });
 
 app.listen(PORT, () => {
